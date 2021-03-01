61149b4eb54e69d39f5e73d386d3d2a7518ff57c
Sanjay LNU <sanjaythammanna@gmail.com>
2021-02-28 19:54:14 -0500

    Added OrderRepository.java

--- orders/src/main/java/com/egen/orders/repository/OrderRepository.java
+++ orders/src/main/java/com/egen/orders/repository/OrderRepository.java
@@ -4,6 +4,7 @@
 package com.egen.orders.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.egen.orders.model.Order;
 
@@ -11,6 +12,8 @@ import com.egen.orders.model.Order;
  * @author sanjaykhan
  *
  */
+
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long>{
 
 }
